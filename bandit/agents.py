2d114a7b49455fe60fff7ebcffdf9e24d32490db
mEskintoch <mustafasamedeskin@gmail.com>
2022-02-22 21:09:11 +0100

    hedge initial commit

--- bandit/agents.py
+++ bandit/agents.py
@@ -276,20 +276,36 @@ class UCB1(Agent):
         chosen_arm.select()
         return chosen_arm.name
 
-    # def choose_arm(self):
-    #     chosen_arm = None
-    #     max_upper_bound = 0
-    #     for arm in self.arms:
-    #         if arm.selections > 0:
-    #             delta_i = math.sqrt(self.confidence * math.log(self.episode+1) / arm.selections)
-    #             upper_bound = arm.mean_reward + delta_i
-    #         else:
-    #             upper_bound = 1e500
-    #         if upper_bound > max_upper_bound:
-    #             max_upper_bound = upper_bound
-    #             chosen_arm = arm
-    #     chosen_arm.select()
-    #     return chosen_arm.name
+    def reward_arm(self, name: str, reward):
+        self.arm(name).reward(reward)
+
+
+class Hedge(Agent):
+    name = 'hedge-bandit'
+
+    def __init__(self,
+                 episodes,
+                 reset_at_end,
+                 temperature: Union[int, float] = 2
+                 ):
+        super().__init__(episodes, reset_at_end)
+        self.temperature = temperature
+
+    def _threshold(self):
+        return sum([math.exp(arm.rewards / self.temperature) for arm in self.arms])
+
+    def choose_arm(self):
+        th = self._threshold()
+        z = random.random()
+        chosen_arm = self.arms[-1] # default
+        p_sum = 0
+        for arm in self.arms:
+            p_sum += math.exp(arm.rewards / self.temperature) / th
+            if p_sum > z:
+                chosen_arm = arm
+                break
+        chosen_arm.select()
+        return chosen_arm.name
 
     def reward_arm(self, name: str, reward):
         self.arm(name).reward(reward)
